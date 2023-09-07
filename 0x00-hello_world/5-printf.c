388269194bb52d9e6d19797ada957c5737174e6a
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-07 15:27:27 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the starting entry function of printing
+ * Return: 0 is passed after printingIt
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
