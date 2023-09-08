98bb29b008ab9449cf347714409fd6ba597ea2df
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-08 16:03:55 -0700

    printalphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main-This is Entry prog for last
+ * Return: it returns last digits 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
