9b91008150d14c347577c1c104747634071db820
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-08 19:55:28 -0700

    7printeralpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - The is the begnn point func
+ * Return: it retrns 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
