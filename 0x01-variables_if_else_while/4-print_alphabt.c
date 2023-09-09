c7f52e1afd39424e8b87a82009a2f93263c6673a
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-08 18:34:51 -0700

    4printalpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the entry program of function
+ * Return: it returns 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
