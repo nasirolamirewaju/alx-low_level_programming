5f349a076bc494a2ee1e5eb499d3f3c0dbb70407
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-08 19:10:37 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - The begn program function
+ * Return: it returns 0
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
