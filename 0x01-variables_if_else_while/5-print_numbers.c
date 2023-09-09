c30e4ec973f37ea502cf13aa7a7f2f5a423527b0
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-08 18:46:26 -0700

    5printnumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is res for entry point of func
+ * Return: it returns 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
