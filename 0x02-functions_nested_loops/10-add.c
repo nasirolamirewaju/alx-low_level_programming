2cbdc6d10a08b0f1ca0ec451e9fffb188e348dd3
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-12 12:37:32 -0700

    äddition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* add - program of sum
+* @a: first parameter
+* @b: second parameter
+* Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
