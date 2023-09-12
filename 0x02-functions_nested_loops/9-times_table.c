ea8eddc75288ed20129dcb16ff573490e7d7e3a8
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-12 12:01:16 -0700

    timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* times_table - multiplication
+* Return: it returns values
+*/
+void times_table(void)
+{
+	int a, b, solution;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			solution = a * b;
+			if (b == 0)
+				printf("%d, ", solution);
+			else
+			{
+				printf("%2d", solution);
+				if (b != 9)
+					printf(",");
+			printf("%2d, ", solution);
+			}
+			printf("\n");
+		}
+	}
+}
