24d7be0cb5c42627c7845e58102ed0ce15480e9c
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-12 14:55:20 -0700

    adder

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -34,11 +34,11 @@ void print_to_98(int n)
 		for (a = n; a >= 98; a--)
 		{
 			printf("%d", a);
+			if (a != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
-	if (a != 98)
-		printf(", ");
 	else if (n == 98)
 		printf("%d\n", n);
 }
