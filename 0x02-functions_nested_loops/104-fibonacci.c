f2a26a7f4d37668a9d3ddf2ba65644e29512506f
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-12 20:10:49 -0700

    fibolimit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - program func first 100 fibo
+* Return: it returns 0
+*/
+int main(void)
+{
+	int a = 0;
+	unsigned long int x = 0, y = 1, next_value = 0;
+
+	while (a < 98)
+	{
+		next_value = x + y;
+		x = y;
+		y = next_value;
+		printf("%lu", next_value);
+		if (a < 97)
+			printf(", ");
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
