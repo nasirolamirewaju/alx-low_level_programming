f36be7d6023ea109ff901646d218896f2018d02d
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-12 21:37:35 -0700

    fibomult

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 /**
-* main - program func first 100 fibo
-* Return: it returns 0
+* main - print the fibo numbers
+* Return: 0
 */
 int main(void)
 {
-	unsigned long int a = 0;
-	unsigned long int x = 0, y = 1, next_value = 0;
+	unsigned long int a = 0, x = 1, b = 0, y = 2;
+	unsigned long int value1, value2, value3;
+	int tally;
 
-	while (a < 98)
+	printf("%lu, %lu, ", x, y);
+	for (tally = 2; tally < 98; tally++)
 	{
-		next_value = x + y;
-		x = y;
-		y = next_value;
-		printf("%lu", next_value);
-		if (a < 97)
+		if (x + y > LARGEST || b > 0 || a > 0)
+		{
+			value1 = (x + y) / LARGEST;
+			value2 = (x + y) % LARGEST;
+			value3 = a + b + value1;
+			a = b, b = value3;
+			x = y, y = value2;
+			printf("%lu%010lu", b, y);
+		}
+		else
+		{
+			value2 = x + y;
+			x = y, y = value2;
+			printf("%lu", y);
+		}
+		if (tally != 97)
 			printf(", ");
-		a++;
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
