61d348328e5d875bf88512ff60f99fbee2c83050
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-14 15:29:43 -0700

    implicit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - print numbers from 0 to 9
+* Return: always 0
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '0'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
