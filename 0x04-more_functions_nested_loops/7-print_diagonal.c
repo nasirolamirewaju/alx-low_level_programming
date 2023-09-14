1a89a1b54fe73945a8006d9315013bc20f7a3013
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-14 16:36:57 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_diagonal: program diagonally parked
+* @n: parameter
+* Return: nothing
+*/
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int b, j;
+
+		for (b = 0; b < n; b++)
+		{
+			for (j = 0; j <= b; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
