a7cc301780d62281acb93e12b511ebcd33623af3
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-14 17:57:17 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_square - program of every square
+* @size: parameter
+* Return: it returns 0
+*/
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
