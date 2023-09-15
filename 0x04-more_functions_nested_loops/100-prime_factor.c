0177f8c467662c8121596b980da9bdeabf0a2aec
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-14 18:39:55 -0700

    prime_numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - program for prime numbers
+* Return: it returns 0
+*/
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
