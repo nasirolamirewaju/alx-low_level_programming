bf8c34352eb72df1999a9ad1bb5d55f2aaac9dec
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-18 16:26:09 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - it programs generated randoms
+* Return: it returns 0
+*/
+
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2272 - sum) - '0' < 78
+		{
+			n = 27772 - sum - '0';
+			sum += n;
+			putchar(n + 0);
+			break;
+			
+		}
+	}
+}
