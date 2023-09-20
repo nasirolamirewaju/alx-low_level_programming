cf31eddbdf55750a851d2851cb274669d49a8a90
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-20 11:46:38 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - it prints magic
+* Return: it returns 0
+*/
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
+
