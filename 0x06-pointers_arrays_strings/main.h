0d60b01f9cec99f14a9197fd5b9a42cc217d1b6e
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-20 09:04:00 -0700

    strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
+#include <string.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
 
 #endif
