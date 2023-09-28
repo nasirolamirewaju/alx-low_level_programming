c6e4e05b231f44125453b422ac37edff85167341
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-09-28 02:56:01 -0700

    100-is_palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,9 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int actual_sqrt_recursion(int n, int i);
 int actual_prime(int n, int i);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 #endif
