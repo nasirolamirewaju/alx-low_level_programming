e7ebf6cf87ad4e754c2b75a07c6dd29fe4799ae4
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-10-03 23:56:44 -0700

    courageous_concatenate

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* str_concat - prog to concatenate two strings
+* @s1: string 1
+* @s2: string 2
+* Return: it points to allocated space
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int s1len = 0;
+	int s2len = 0;
+	int j;
+	char *output;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (j = 0; s1[j] != '\0'; j++)
+		s1len++;
+	for (j = 0; s2[j] != '\0'; j++)
+		s2len++;
+
+	output = malloc(sizeof(char) * (s1len + s2len) + 1);
+	if (output == NULL)
+		return (NULL);
+	for (j = 0; s1[j] != '\0'; j++)
+		output[j] = s1[j];
+	for (j = 0; s2[j] != '\0'; j++)
+		output[s1len + 1] = s2[j];
+	return (output);
+}
