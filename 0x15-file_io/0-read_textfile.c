bf8a8d8e510e09caa0cdf592a827806a95ecc546
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-10-30 03:24:57 -0700

    readTextfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfiles - this program reads a text file to print POSIX
+ * @filename: parameter for filename to read
+ * @letters: parameters to read and print number of letters
+ * Return: it returns actual number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_d;
+	ssize_t lenr, lenw;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	file_d = open(filename, O_RDONLY);
+	if (file_d == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_d);
+		return (0);
+	}
+	lenr = read(file_d, buffer, letters);
+	close(file_d);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
+}
