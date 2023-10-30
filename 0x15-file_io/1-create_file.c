f95111651689b1d840c911552ec520f9d75211af
nasirolamirewaju <nasirolamirewaju10@gmail.com>
2023-10-30 04:04:13 -0700

    CreatFile

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - program that creates a file
+ * @filename: parameter for filename
+ * @text_content: parameter for content to be written in the file
+ * Return: it returns 1 if successful and -1 if it fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_d;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_d == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+	rwr = write(file_d, text_content, nletters);
+	if (rwr == -1)
+		return (-1);
+	close(file_d);
+	return (1);
+}
